dcb8916fec63f5ed1a789160c14b86be2e093840
EduardLupu <lupu.edward@yahoo.com>
2025-04-23 22:31:01 +0300

    Added client, server and docker-compose.yml

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # diss-project
+
+Please run the docker-compose command to start the project.
+
+```bash
+
+docker compose up
+```
+
+If you want to run the project in the background, you can use the `-d` flag:
+
+```bash
+
+docker compose up -d
+```
+
+If you want to do development, you can simply just run the postgres container.
---
+++ server/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.diss"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
