d4df78e049155e83cdd0421a5b6a9e49e77c9183
EduardLupu <lupu.edward@yahoo.com>
2025-04-26 22:55:16 +0300

    Changed next.js config.

---
+++ client/next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      canvas: false,
+    };
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/pdfs/:path*',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/pdf',
+          },
+        ],
+      },
+    ];
+  },
+}
+
+module.exports = nextConfig 
