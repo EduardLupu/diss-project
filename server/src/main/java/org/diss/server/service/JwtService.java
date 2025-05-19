0305fd2dd9f41bd4674ce6ba06988927b10eda05
Kun Alexandru <alexandru.kun@stud.ubbcluj.ro>
2025-05-19 23:25:29 +0300

    updated jwt to last for 4 weeks

--- server/src/main/java/org/diss/server/service/JwtService.java
+++ server/src/main/java/org/diss/server/service/JwtService.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
     private String secretKey = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
-    private long jwtExpiration = 604800000;
+    private long jwtExpiration = 2419200000L;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
