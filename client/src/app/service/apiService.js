cf8fc9cb81fe5108551e071210e0d9c0342251d6
Kun Alexandru <alexandru.kun@stud.ubbcluj.ro>
2025-05-25 03:37:56 +0300

    Fixed api service base url

--- client/src/app/service/apiService.js
+++ client/src/app/service/apiService.js
@@ -1,6 +1,6 @@
 // apiService.js
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://api.eduwave.eduardlupu.com';
 /**
  * API service for handling requests to the backend
  */
